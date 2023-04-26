46d62f7b724358ee4b78fb2e9c181a31762fc073
elinagorniker <elinagorniker@gmail.com>
2023-04-26 14:14:27 +0200

    1.Encoding of the password 2.Adding class Utils

---
+++ src/main/java/utils/Utils.java
@@ -0,0 +1,12 @@
+package utils;
+
+import java.util.Base64;
+
+public class Utils {
+    public static String decode64(String encodeStr) {
+        Base64.Decoder decoder = Base64.getDecoder();
+        return new String(decoder.decode(encodeStr.getBytes()));
+
+    }
+
+}
